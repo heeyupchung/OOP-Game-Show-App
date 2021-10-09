135f99f61e0e43be176ce01e9448dd48c4a57f51
heeyupchung <heeyupchung@gmail.com>
2021-10-09 13:34:14 -0700

    finished class Phrase

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,41 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor(missed = 0, activePhrase = null) {
+        this.missed = missed;
+        this.phrases = [
+            new Phrase('Code on'), 
+            new Phrase('You got this'), 
+            new Phrase('Never give up'), 
+            new Phrase('Doing great'), 
+            new Phrase('How are you') 
+        ];
+        this.activePhrase = this.getRandomPhrase;
+    }
+
+    startGame() {
+
+    }
+
+    getRandomPhrase() {
+
+    }
+
+    handleInteraction() {
+
+    }
+
+    removeLife() {
+
+    }
+
+    checkForWin() {
+
+    }
+
+    gameOver() {
+
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,47 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay() {
+        for (let i = 0; i < this.phrase.length; i++) {
+            let letter = document.createElement('li');
+            letter.innerHTML = `${this.phrase[i]}`;
+            if (this.phrase[i] === ' ') {
+                letter.className = 'space';
+            } else {
+                letter.className = `hide letter ${this.phrase[i]}`;
+            }
+            $('#phrase ul').appendChild('li');
+        }
+        
+        if (this.checkLetter()) {
+            this.showMatchedLetter();
+        }
+    }
+
+    checkLetter() {
+        document.addEventListener('keydown', e => {
+            for (let i = 0; i < this.phrase.length; i++) {
+                if (this.phrase[i] === e.key) {
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+
+    showMatchedLetter() {
+        document.addEventListener('keyup', e => {
+            let matchedLetter = document.getElementsByClassName(`${e.key}`);
+            for (let i = 0; i < matchedLetter.length; i++) {
+                matchedLetter[i].classList.remove('hide');
+                matchedLetter[i].classList.add('show');
+            }
+        });
+    }
+}
